d1df98fd4f62c5de5d757ba2603c9932969f531d
Daniel Mateu <danielmateu86@gmail.com>
2023-01-17 15:17:28 +0100

    Configuración de redux...

--- crud-redux/src/App.jsx
+++ crud-redux/src/App.jsx
@@ -1,22 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { EditarProducto, Header, NuevoProducto, Productos } from './components';
 
+//Redux
+import { Provider } from 'react-redux'
+import store from './store';
+
+
 function App() {
 
 
   return (
     <Router>
-      <Header />
-      <div className="px-40 py-10">
-
-        <Routes>
-          <Route exact path="/" element={<Productos />} />
-          <Route exact path="/productos/nuevo" element={<NuevoProducto />} />
-          <Route exact path="/productos/editar/:id" element={<EditarProducto />} />
-        </Routes>
-      </div>
-
-      {/* <Productos /> */}
+      <Provider store={store}>
+        <Header />
+        <div className="px-40 py-10">
+          <Routes>
+            <Route exact path="/" element={<Productos />} />
+            <Route exact path="/productos/nuevo" element={<NuevoProducto />} />
+            <Route exact path="/productos/editar/:id" element={<EditarProducto />} />
+          </Routes>
+        </div>
+      </Provider>
     </Router>
 
   )
